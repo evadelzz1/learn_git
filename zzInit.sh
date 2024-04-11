b8b5c0ce96dc61d78b1c168ebb8417d0085cc8e6
evadelzz1 <evadel.zz1@gmail.com>
2024-04-11 16:20:14 -0700

    update

---
+++ zzInit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '
+pwd
+
+cp -rf ./* ./../learn_git_test/
+'
+
+
+echo ">>> this dir is ..."
+pwd
+
+echo ">>> Copy a files from learn_git to learn_git_test..."
+cp -rf ./* ./../learn_git_test/
+
